38f2d27615c477ac1ab1f8da8c206a1c7dfb7178
Fadi <fadybd1@gmail.com>
2022-07-21 19:44:54 +0300

    Added Navigation Bar + Animation.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//fetch quote using API
 async function fetchQuote() {
     const url = 'https://type.fit/api/quotes';
     try {
@@ -21,7 +22,7 @@ async function generateQuote() {
         const quoteText = quotes[randomQuoteIndex].text;
         let author = quotes[randomQuoteIndex].author;
         if (!author) {
-            author = '-Unknown';
+            author = 'Unknown';
         }
 
         (function (next) {
@@ -40,6 +41,7 @@ async function generateQuote() {
 
 generateQuote();
 
+//Fade quote author and text in
 async function unfade(element) {
     var op = 0.1;  // initial opacity
     element.style.display = 'block';
@@ -52,3 +54,50 @@ async function unfade(element) {
         op += op * 0.1;
     }, 100);
 }
+
+// Highlight words in the quote section
+quoteEl.onmouseover = e => {
+    e.target.innerHTML = e.target.innerText.replace(/([\w]+)/g, '<span>$1</span>');
+};
+
+
+
+(function () {
+
+    const link = document.querySelectorAll('nav > .navBar-item-hover');
+    const cursor = document.querySelector('.cursor');
+
+    const animateit = function (e) {
+        const span = this.querySelector('span');
+        const { offsetX: x, offsetY: y } = e,
+            { offsetWidth: width, offsetHeight: height } = this,
+
+            move = 25,
+            xMove = x / width * (move * 2) - move,
+            yMove = y / height * (move * 2) - move;
+
+        span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+        if (e.type === 'mouseleave') span.style.transform = '';
+    };
+
+    const editCursor = e => {
+        const { clientX: x, clientY: y } = e;
+        cursor.style.left = x + 'px';
+        cursor.style.top = y + 'px';
+    };
+
+    link.forEach(b => b.addEventListener('mousemove', animateit));
+    link.forEach(b => b.addEventListener('mouseleave', animateit));
+    window.addEventListener('mousemove', editCursor);
+
+})();
+
+// const nameTitle2 = document.querySelector('.name-title2');
+// nameTitle2.classList.remove(".name-title2");
+// nameTitle2.offsetWidth;
+
+// setTimeout(() => {
+//     nameTitle2.style.visibility = 'visible';
+//     element.classList.add(".name-title2");
+// }, 5000); 
